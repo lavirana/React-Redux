1e336861088223b83deea9a5dfb137b120b7076c
Ashish Rana <ashishrana2882gmail.com>
2024-11-17 16:59:47 +0530

    add html css

---
+++ src/components/Home.js
@@ -0,0 +1,110 @@
+import React from "react";
+
+function Home()
+{
+    return(
+    <div>
+        <div className="add-to-cart">
+            <img src="https://assets.dryicons.com/uploads/icon/svg/5575/537345a6-062b-401f-aba0-79c87a1037b1.svg" />
+        </div>
+        <h1>Home</h1>
+          <div className="cart-wrapper">
+            <div className="img-wrapper item">
+             <img src="https://tvguru.co.ke/wp-content/uploads/2021/08/Vitron-HTC4068S-40-Inch-Smart-TV-430x430.jpg" />
+            </div>
+            <div className="text-wrapper item">
+                <span>
+                    Television
+                </span>
+                <span>
+                    Price: $1000.00
+                </span>
+            </div>
+            <div className="btn-wrapper item">
+                <button>Add to cart</button>
+            </div>
+         </div>
+         <div className="cart-wrapper">
+            <div className="img-wrapper item">
+             <img src="https://tvguru.co.ke/wp-content/uploads/2021/08/Vitron-HTC4068S-40-Inch-Smart-TV-430x430.jpg" />
+            </div>
+            <div className="text-wrapper item">
+                <span>
+                    Television
+                </span>
+                <span>
+                    Price: $1000.00
+                </span>
+            </div>
+            <div className="btn-wrapper item">
+                <button>Add to cart</button>
+            </div>
+         </div>
+         <div className="cart-wrapper">
+            <div className="img-wrapper item">
+             <img src="https://tvguru.co.ke/wp-content/uploads/2021/08/Vitron-HTC4068S-40-Inch-Smart-TV-430x430.jpg" />
+            </div>
+            <div className="text-wrapper item">
+                <span>
+                    Television
+                </span>
+                <span>
+                    Price: $1000.00
+                </span>
+            </div>
+            <div className="btn-wrapper item">
+                <button>Add to cart</button>
+            </div>
+         </div>
+         <div className="cart-wrapper">
+            <div className="img-wrapper item">
+             <img src="https://tvguru.co.ke/wp-content/uploads/2021/08/Vitron-HTC4068S-40-Inch-Smart-TV-430x430.jpg" />
+            </div>
+            <div className="text-wrapper item">
+                <span>
+                    Television
+                </span>
+                <span>
+                    Price: $1000.00
+                </span>
+            </div>
+            <div className="btn-wrapper item">
+                <button>Add to cart</button>
+            </div>
+         </div>
+         <div className="cart-wrapper">
+            <div className="img-wrapper item">
+             <img src="https://tvguru.co.ke/wp-content/uploads/2021/08/Vitron-HTC4068S-40-Inch-Smart-TV-430x430.jpg" />
+            </div>
+            <div className="text-wrapper item">
+                <span>
+                    Television
+                </span>
+                <span>
+                    Price: $1000.00
+                </span>
+            </div>
+            <div className="btn-wrapper item">
+                <button>Add to cart</button>
+            </div>
+         </div>
+         <div className="cart-wrapper">
+            <div className="img-wrapper item">
+             <img src="https://tvguru.co.ke/wp-content/uploads/2021/08/Vitron-HTC4068S-40-Inch-Smart-TV-430x430.jpg" />
+            </div>
+            <div className="text-wrapper item">
+                <span>
+                    Television
+                </span>
+                <span>
+                    Price: $1000.00
+                </span>
+            </div>
+            <div className="btn-wrapper item">
+                <button>Add to cart</button>
+            </div>
+         </div>
+    </div>
+    )
+}
+export default Home;
